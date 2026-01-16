4bfbe7ebb3adb3b3944cef1eed27991143ceb99a
aleph <aleph@aleph.mx>
2026-01-15 19:55:25 -0700

    update info

--- site/next.config.js
+++ site/next.config.js
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export',
-  distDir: '../public',
+  output: "export",
+  distDir: "../public",
   env: {
-    name: 'Aleph Registry',
-    description: 'The aleph workspaces.',
-    icon: 'https://img.icons8.com/?size=100&id=53372&format=png&color=000000',
-    listUrl: 'https://github.com/alephramos/aleph-registry/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: "Aleph Registry",
+    description: "The aleph workspaces.",
+    icon: "https://img.icons8.com/?size=100&id=53372&format=png&color=000000",
+    listUrl: "https://github.com/alephramos/aleph-registry/",
+    contactUrl: "https://github.com/alephramos/aleph-registry/issues",
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+  swcMinify: true,
+  basePath: "/kasm-registry/1.1",
   trailingSlash: true,
   images: {
     unoptimized: true,
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
